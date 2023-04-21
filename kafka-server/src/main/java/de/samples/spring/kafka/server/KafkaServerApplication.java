1ea25fd776c1fd0797195b8e133eaac4fbd54310
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2023-04-21 13:51:20 +0200

    Initialize Kafka Server Module

---
+++ kafka-server/src/main/java/de/samples/spring/kafka/server/KafkaServerApplication.java
@@ -0,0 +1,13 @@
+package de.samples.spring.kafka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaServerApplication.class, args);
+    }
+
+}
---
+++ kafka-server/src/test/java/de/samples/spring/kafka/server/KafkaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.samples.spring.kafka.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
